9382a4ae3add006b0a5f01eee7e10722d813df1b
Pepijn Kenter <titusjan@gmail.com>
2013-02-03 12:52:00 +0100

    Refactoring

--- libimg.py
+++ libimg.py
@@ -37,52 +37,55 @@ def qimg_to_array(img):
         "size mismatch: {} != {}".format(n_bits_buffer, n_bits_image) 
 
     if img.format() == QtGui.QImage.Format.Format_RGB32:  
-        arr = np.ndarray(shape=(img_size.width(), img_size.height()), buffer=buffer, dtype=DTYPE_RGB32_LE)
+        arr_rgba = np.ndarray(shape=(img_size.width(), img_size.height()), buffer=buffer, dtype=DTYPE_RGB32_LE)
     elif img.format() == QtGui.QImage.Format.Format_ARGB32:  
-        arr = np.ndarray(shape=(img_size.width(), img_size.height()), buffer=buffer, dtype=DTYPE_ARGB32_LE)
+        arr_rgba = np.ndarray(shape=(img_size.width(), img_size.height()), buffer=buffer, dtype=DTYPE_ARGB32_LE)
     else:
         assert False, "Format not implemented: {}".format(img.format()) 
 
-    return arr
+    return arr_rgba
     
     
-    
-def rgba_array_to_depth4(arr_argb32):
+def rgba_array_to_depth4(arr_rgba):
 
     """ Creates (width x height x depth) array, where the depth = 4 (rgba).
     
         This can be used in matplot
     """
-    width, height = arr_argb32.shape
+    width, height = arr_rgba.shape
     arr4 = np.ndarray(shape=(width, height, 4), dtype = np.uint8)
         
-    arr4[:,:,0] = arr_argb32[:]['r']
-    arr4[:,:,1] = arr_argb32[:]['g']
-    arr4[:,:,2] = arr_argb32[:]['b']
-    arr4[:,:,3] = arr_argb32[:]['a']
+    arr4[:,:,0] = arr_rgba[:]['r']
+    arr4[:,:,1] = arr_rgba[:]['g']
+    arr4[:,:,2] = arr_rgba[:]['b']
+    arr4[:,:,3] = arr_rgba[:]['a']
     
     return arr4
     
 
-        
-        
+def save_rgba_array_fo_file(file_name, arr_rgba):
+    """ Saves an rbga array to file
+    """
+    arr4 = rgba_array_to_depth4(arr_rgba)
+    mpimg.imsave(file_name, arr4, vmin=0, vmax=255)        
+    
 
 def test():
 
     img = QtGui.QImage("images/mona_lisa_300x300.png")
     
-    arr_argb32 = qimg_to_array(img)
-    arr4 = rgba_array_to_depth4(arr_argb32)
-
-    mpimg.imsave('mona_lisa_out.png', arr4, vmin=0, vmax=255)
+    arr_rgba = qimg_to_array(img)
+    save_rgba_array_fo_file('mona_lisa_out.png', arr_rgba)
     
+
     
  
 def main():
 
     logging.basicConfig(level = 'DEBUG', 
-        format='%(filename)20s:%(lineno)-4d : %(levelname)-7s: %(message)s')
-
+        #format='%(filename)20s:%(lineno)-4d : %(levelname)-7s: %(message)s')
+        format='%(asctime)s: %(filename)20s:%(lineno)-4d : %(levelname)-6s: %(message)s')
+        
     logger.info('Started {}'.format(__name__))
     test()
     logger.info('Done {}'.format(__name__))
