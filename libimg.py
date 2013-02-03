2229026017c9813f3440f0e7944bf73b8812cf50
Pepijn Kenter <titusjan@gmail.com>
2013-02-03 14:36:23 +0100

    Using width x height x depth arrays instead of record arrays. Implemented image_array_average and image_array_abs_diff

--- libimg.py
+++ libimg.py
@@ -5,6 +5,7 @@
 
 """
 from __future__ import print_function
+from __future__ import division
 
 import logging
 logger = logging.getLogger(__name__)
@@ -15,15 +16,32 @@ import matplotlib.image as mpimg
 from PySide import QtCore, QtGui
 from PySide.QtCore import Qt
 
-        
-# Define dtypes for the different image formats (LE = little endian).
-# ARGB32 is slow to render on a painter, use either RGB32 or ARGB32_Premultiplied
 
-DTYPE_RGB32_LE = [('b', np.uint8), ('g', np.uint8), ('r', np.uint8), ('a', np.uint8)]
-DTYPE_ARGB32_LE = DTYPE_RGB32_LE
+# Define constants for the depth dimension when an image is converted to a Width x Height x Depth array
+# Qt uses ARGB (or BGRA in little endian) when the format is RGB32, ARGB32 or ARGB32_Premultiplied
+
+QT_LITTLE_ENDIAN_MODE = True
+assert QT_LITTLE_ENDIAN_MODE, "Big endian mode not tested yet"
+
+if QT_LITTLE_ENDIAN_MODE:
+    QT_DEPTH_B = 0
+    QT_DEPTH_G = 1
+    QT_DEPTH_R = 2
+    QT_DEPTH_A = 3
+else:
+    QT_DEPTH_A = 0
+    QT_DEPTH_R = 1
+    QT_DEPTH_G = 2
+    QT_DEPTH_B = 3
+    
+# MatPlotLib uses RGBA when saving an image. 
+MPL_DEPTH_R = 0
+MPL_DEPTH_G = 1
+MPL_DEPTH_B = 2
+MPL_DEPTH_A = 3
 
 
-def qimg_to_array(img):
+def qt_image_to_array(img):
     """ Creates a numpy array from a QImage.
     """
     img_size = img.size()
@@ -35,47 +53,73 @@ def qimg_to_array(img):
     n_bits_image  = img_size.width() * img_size.height() * img.depth()
     assert n_bits_buffer == n_bits_image, \
         "size mismatch: {} != {}".format(n_bits_buffer, n_bits_image) 
-
-    if img.format() == QtGui.QImage.Format.Format_RGB32:  
-        arr_rgba = np.ndarray(shape=(img_size.width(), img_size.height()), buffer=buffer, dtype=DTYPE_RGB32_LE)
-    elif img.format() == QtGui.QImage.Format.Format_ARGB32:  
-        arr_rgba = np.ndarray(shape=(img_size.width(), img_size.height()), buffer=buffer, dtype=DTYPE_ARGB32_LE)
-    else:
-        assert False, "Format not implemented: {}".format(img.format()) 
-
-    return arr_rgba
-    
-    
-def rgba_array_to_depth4(arr_rgba):
-
-    """ Creates (width x height x depth) array, where the depth = 4 (rgba).
-    
-        This can be used in matplot
-    """
-    width, height = arr_rgba.shape
-    arr4 = np.ndarray(shape=(width, height, 4), dtype = np.uint8)
         
-    arr4[:,:,0] = arr_rgba[:]['r']
-    arr4[:,:,1] = arr_rgba[:]['g']
-    arr4[:,:,2] = arr_rgba[:]['b']
-    arr4[:,:,3] = arr_rgba[:]['a']
-    
-    return arr4
-    
+    assert img.depth() == 32, "unexpected image depth: {}".format(img.depth())
+        
+    return np.ndarray(shape  = (img_size.width(), img_size.height(), img.depth()//8), 
+                      buffer = buffer, 
+                      dtype  = np.uint8)
+  
 
-def save_rgba_array_fo_file(file_name, arr_rgba):
-    """ Saves an rbga array to file
+
+def qt_arr_to_mpl_arr(arr_qt):
+    """ Converts array originating from a QT image to an image to use in matplotlib.imsave()
+    
+        Permutates the depth dimension of a (w,h,d=4) array from ARGB/BGRA to RGBA
     """
-    arr4 = rgba_array_to_depth4(arr_rgba)
-    mpimg.imsave(file_name, arr4, vmin=0, vmax=255)        
+    
+    """ Saves a (width x height x depth) array to file
+    """
+    arr_mpl = np.ndarray(shape = arr_qt.shape, dtype = np.uint8)
+    
+    arr_mpl[:,:,MPL_DEPTH_R] = arr_qt[:,:,QT_DEPTH_R]
+    arr_mpl[:,:,MPL_DEPTH_G] = arr_qt[:,:,QT_DEPTH_G]
+    arr_mpl[:,:,MPL_DEPTH_B] = arr_qt[:,:,QT_DEPTH_B]
+    arr_mpl[:,:,MPL_DEPTH_A] = arr_qt[:,:,QT_DEPTH_A]
+    
+    return arr_mpl
+
+def save_qt_img_array_fo_file(file_name, arr_qt):
+    """ Saves (w,h,d=4) array (with depth in Qt order) to a file
+    """
+    arr_mpl = qt_arr_to_mpl_arr(arr_qt)
+    mpimg.imsave(file_name, arr_mpl, vmin=0, vmax=255)        
+    
+ 
+def image_array_average(arr1, arr2):
+    """ Returns (arr1-arr2)/2 for unsigned integers
+    """
+    # First devide by two to prevent overflow
+    return arr1 // 2 + arr2 // 2
+
+def image_array_abs_diff(arr1, arr2):
+    """ Returns abs(arr1-arr2) for unsigned integers
+    """
+    diff = np.where( np.greater_equal(arr1, arr2), arr1-arr2, arr2-arr1)
+    diff[:,:,QT_DEPTH_A] = 255
+    return diff
+    
+    
+def test1():
+
+    #img = QtGui.QImage("images/mona_lisa_300x300.jpg")
+    img = QtGui.QImage("images/mona_sister_300x300.jpg")
+    
+    arr = qt_image_to_array(img)
+    save_qt_img_array_fo_file('mona_lisa_out.png', arr)
     
 
 def test():
 
-    img = QtGui.QImage("images/mona_lisa_300x300.png")
+    img1 = QtGui.QImage("images/mona_lisa_300x300.jpg")
+    arr1 = qt_image_to_array(img1)
+    img2 = QtGui.QImage("images/mona_sister_300x300.jpg")
+    arr2 = qt_image_to_array(img2)
+   
+    #result = image_array_average(arr1, arr2)
+    result = image_array_abs_diff(arr1, arr2) 
     
-    arr_rgba = qimg_to_array(img)
-    save_rgba_array_fo_file('mona_lisa_out.png', arr_rgba)
+    save_qt_img_array_fo_file('mona_lisa_out.png', result)
     
 
     
